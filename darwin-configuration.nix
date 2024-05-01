1cbd538c3a9586446d8fcb9e626a470bbd5b5c7e
James Birtles <james@birtles.dev>
2024-05-01 10:19:05 +0100

    fix indentation

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -6,22 +6,22 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-  	neovim
-	nodejs_20
-	zoxide
-	git
-	gnupg
-	gh
-	ripgrep
-	fd
-	lazygit
-	ansible
-	pandoc
-	hcloud
-	bun
-	rustup
-	texliveSmall
-	wrangler
+    neovim
+    nodejs_20
+    zoxide
+    git
+    gnupg
+    gh
+    ripgrep
+    fd
+    lazygit
+    ansible
+    pandoc
+    hcloud
+    bun
+    rustup
+    texliveSmall
+    wrangler
   ];
 
   # Use a custom configuration.nix location.
@@ -38,8 +38,8 @@
   system.stateVersion = 4;
 
   users.users.james = {
-	name = "james";
-	home = "/Users/james";
+  name = "james";
+  home = "/Users/james";
   };
   home-manager.useGlobalPkgs = true;
   home-manager.users.james = { pkgs, ... }: {
@@ -48,54 +48,54 @@
     xdg.enable = true;
 
     programs.zsh.enable = true;
-	programs.zsh.enableAutosuggestions = true;
-	programs.oh-my-posh.enable = true;
-	programs.oh-my-posh.useTheme = "half-life";
-	programs.zsh.syntaxHighlighting.enable = true;
+    programs.zsh.enableAutosuggestions = true;
+    programs.oh-my-posh.enable = true;
+    programs.oh-my-posh.useTheme = "half-life";
+    programs.zsh.syntaxHighlighting.enable = true;
 
-	programs.neovim.enable = true;
-	programs.neovim.defaultEditor = true;
-	programs.neovim.viAlias = true;
-	programs.neovim.vimAlias = true;
-	programs.neovim.vimdiffAlias = true;
+    programs.neovim.enable = true;
+    programs.neovim.defaultEditor = true;
+    programs.neovim.viAlias = true;
+    programs.neovim.vimAlias = true;
+    programs.neovim.vimdiffAlias = true;
 
-	programs.wezterm.enable = true;
-	programs.wezterm.extraConfig = ''
-	  local wezterm = require("wezterm")
+    programs.wezterm.enable = true;
+    programs.wezterm.extraConfig = ''
+      local wezterm = require("wezterm")
 
-	  local config = wezterm.config_builder()
+      local config = wezterm.config_builder()
 
-	  config.color_scheme = "Ayu Dark (Gogh)"
-	  config.font = wezterm.font("Berkeley Mono Variable")
-	  config.font_size = 18.0
-	  config.use_fancy_tab_bar = false
-	  config.hide_tab_bar_if_only_one_tab = true
-	  config.tab_bar_at_bottom = true
+      config.color_scheme = "Ayu Dark (Gogh)"
+      config.font = wezterm.font("Berkeley Mono Variable")
+      config.font_size = 18.0
+      config.use_fancy_tab_bar = false
+      config.hide_tab_bar_if_only_one_tab = true
+      config.tab_bar_at_bottom = true
 
-	  return config
-	'';
+      return config
+    '';
 
-	programs.git.enable = true;
-	programs.git.userEmail = "james@birtles.dev";
-	programs.git.userName = "James Birtles";
-	programs.git.signing.signByDefault = true;
-	programs.git.signing.key = null;
-	programs.git.extraConfig.push.autoSetupRemote = true;
-	programs.git.extraConfig.init.defaultBranch = "main";
-	programs.git.ignores = [
-	  ".DS_Store"
-	  "Desktop.ini"
-	  ".Spotlight-V100"
-	  ".Trashes"
-	];
+    programs.git.enable = true;
+    programs.git.userEmail = "james@birtles.dev";
+    programs.git.userName = "James Birtles";
+    programs.git.signing.signByDefault = true;
+    programs.git.signing.key = null;
+    programs.git.extraConfig.push.autoSetupRemote = true;
+    programs.git.extraConfig.init.defaultBranch = "main";
+    programs.git.ignores = [
+      ".DS_Store"
+      "Desktop.ini"
+      ".Spotlight-V100"
+      ".Trashes"
+    ];
 
-	programs.lazygit.enable = true;
-	programs.lazygit.settings.gui.showFileTree = false;
+    programs.lazygit.enable = true;
+    programs.lazygit.settings.gui.showFileTree = false;
   };
 
   programs.zsh.enable = true;
   programs.zsh.interactiveShellInit = ''
-	eval "$(zoxide init --cmd cd zsh)"
+  eval "$(zoxide init --cmd cd zsh)"
   '';
 
   environment.loginShell = "${pkgs.zsh}/bin/zsh -l";
@@ -108,36 +108,36 @@
   homebrew.enable = true;
   # todo: these should probably almost all be moved to using nix packages
   homebrew.brews = [
-	"chezmoi"
-	"pinentry-mac"
+    "chezmoi"
+    "pinentry-mac"
   ];
   homebrew.casks = [
-	"amethyst"
-	"wezterm"
-	"basictex"
-	"unnaturalscrollwheels"
-	"visual-studio-code"
-	"postico"
-	"betterdisplay"
-	"caffeine"
-	"cleanshot"
-  	"displaylink"
-	"firefox"
-	"google-chrome"
-	"microsoft-teams"
-	"microsoft-auto-update"
-	"obs"
-	"orbstack"
-	"postman"
-	"qmk-toolbox"
-	"spotify"
-	"vial"
-	"vlc"
-	"whatsapp"
+    "amethyst"
+    "wezterm"
+    "basictex"
+    "unnaturalscrollwheels"
+    "visual-studio-code"
+    "postico"
+    "betterdisplay"
+    "caffeine"
+    "cleanshot"
+    "displaylink"
+    "firefox"
+    "google-chrome"
+    "microsoft-teams"
+    "microsoft-auto-update"
+    "obs"
+    "orbstack"
+    "postman"
+    "qmk-toolbox"
+    "spotify"
+    "vial"
+    "vlc"
+    "whatsapp"
   ];
   homebrew.masApps = {
-	# To use the biometrics with the browser plugin, the app needs to be installed via the mac app store
-	Bitwarden = 1352778147;
+    # To use the biometrics with the browser plugin, the app needs to be installed via the mac app store
+    Bitwarden = 1352778147;
   };
   homebrew.onActivation.cleanup = "zap";
   homebrew.onActivation.autoUpdate = true;
@@ -145,12 +145,12 @@
 
 
   networking.dns = [
-	"1.1.1.1"
-	"1.0.0.1"
+    "1.1.1.1"
+    "1.0.0.1"
   ];
   networking.knownNetworkServices = [
-	"Wi-Fi"
-	"USB 10/100/1G/2.5G LAN"
+    "Wi-Fi"
+    "USB 10/100/1G/2.5G LAN"
   ];
 
   programs.gnupg.agent.enable = true;
