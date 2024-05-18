73139db849a66f0475039273e3e8d65d371bebaf
James Birtles <james@birtles.dev>
2024-05-18 12:46:12 +0100

    discord

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -136,6 +136,7 @@
     "steam"
     "royal-tsx"
     "notunes"
+    "discord"
   ];
   homebrew.masApps = {
     # To use the biometrics with the browser plugin, the app needs to be installed via the mac app store
