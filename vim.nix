89b7aef4b91c90f9debd73a55eb7537af31cf267
James Birtles <james@birtles.dev>
2024-05-30 13:08:58 +0100

    only do completion if explicitly selected

--- vim.nix
+++ vim.nix
@@ -144,7 +144,7 @@
             "<C-d>" = "cmp.mapping.scroll_docs(-4)";
             "<C-e>" = "cmp.mapping.close()";
             "<C-f>" = "cmp.mapping.scroll_docs(4)";
-            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<CR>" = "cmp.mapping.confirm({ select = false })";
             "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
             "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
           };
