85d80dc48bc6e3d1a6a5e17889e8b106716a51e5
James Birtles <james@birtles.dev>
2024-06-01 14:36:17 +0100

    yabai

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -127,4 +127,74 @@
       };
     };
   };
+
+  services.yabai = {
+    enable = true;
+    enableScriptingAddition = true;
+    config = let
+      padding = 8;
+    in {
+      layout = "bsp";
+      window_placement = "second_child";
+      top_padding = padding;
+      bottom_padding = padding;
+      left_padding = padding;
+      right_padding = padding;
+      window_gap = padding;
+      mouse_follows_focus = "on";
+      mouse_modifier = "alt";
+      # alt + left click + drag
+      mouse_action1 = "move";
+      # alt + right click + drag
+      mouse_action2 = "resize";
+      mouse_drop_action = "swap";
+    };
+    extraConfig = ''
+      yabai -m rule --add app='^System Settings$' manage=off
+      yabai -m rule --add app='^Calculator$' manage=off
+    '';
+  };
+  environment.systemPackages = [pkgs.skhd];
+  services.skhd = {
+    enable = true;
+    skhdConfig = ''
+      alt - j : yabai -m window --focus south
+      alt - k : yabai -m window --focus north
+      alt - h : yabai -m window --focus west
+      alt - l : yabai -m window --focus east
+      alt - n : yabai -m display --focus east
+      alt - p : yabai -m display --focus west
+
+      shift + alt - j : yabai -m space --mirror x-axis
+      shift + alt - h : yabai -m space --mirror y-axis
+      shift + alt - l : yabai -m space --rotate 270
+      shift + alt - f : yabai -m window --toggle float --grid 4:4:1:1:2:2
+
+      shift + alt - m : yabai -m window --toggle zoom-fullscreen
+      shift + alt - b : yabai -m space --balance
+
+      cmd + alt - j : yabai -m window --swap south
+      cmd + alt - k : yabai -m window --swap north
+      cmd + alt - l : yabai -m window --swap east
+      cmd + alt - h : yabai -m window --swap west
+      cmd + alt - n : yabai -m window --display east; yabai -m display --focus east
+      cmd + alt - j : yabai -m window --display west; yabai -m display --focus west
+
+      cmd + ctrl + alt - j : yabai -m window --warp south
+      cmd + ctrl + alt - k : yabai -m window --warp north
+      cmd + ctrl + alt - l : yabai -m window --warp east
+      cmd + ctrl + alt - h : yabai -m window --warp west
+
+      shift + cmd + alt - l : yabai -m window --space next
+      shift + cmd + alt - h : yabai -m window --space prev
+
+      shift + cmd + alt - 1 : yabai -m window --space 1
+      shift + cmd + alt - 2 : yabai -m window --space 2
+      shift + cmd + alt - 3 : yabai -m window --space 3
+      shift + cmd + alt - 4 : yabai -m window --space 4
+      shift + cmd + alt - 5 : yabai -m window --space 5
+      shift + cmd + alt - 6 : yabai -m window --space 6
+      shift + cmd + alt - 7 : yabai -m window --space 7
+    '';
+  };
 }
