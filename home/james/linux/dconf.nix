f8c85eb8d87228f7bf4c463d7c29aba9bf373ec4
James Birtles <james@birtles.dev>
2024-09-21 14:13:08 +0100

    dconf

--- home/james/linux/dconf.nix
+++ home/james/linux/dconf.nix
@@ -6,8 +6,31 @@ with lib.hm.gvariant;
 {
   dconf.settings = {
     "org/gnome/control-center" = {
-      last-panel = "background";
-      window-state = mkTuple [ 1508 1034 false ];
+      last-panel = "display";
+      window-state = mkTuple [ 1503 970 true ];
+    };
+
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [ "Utilities" "YaST" "Pardus" ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/Pardus" = {
+      categories = [ "X-Pardus-Apps" ];
+      name = "X-Pardus-Apps.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = [ "gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.fonts.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" "vinagre.desktop" ];
+      categories = [ "X-GNOME-Utilities" ];
+      name = "X-GNOME-Utilities.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/YaST" = {
+      categories = [ "X-SuSE-YaST" ];
+      name = "suse-yast.directory";
+      translate = true;
     };
 
     "org/gnome/desktop/background" = {
@@ -20,6 +43,7 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/input-sources" = {
+      sources = [ (mkTuple [ "xkb" "gb" ]) ];
       xkb-options = [ "caps:escape" ];
     };
 
@@ -30,7 +54,11 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/notifications" = {
-      application-children = [ "org-gnome-settings" ];
+      application-children = [ "org-gnome-settings" "org-gnome-nautilus" ];
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {
+      application-id = "org.gnome.Nautilus.desktop";
     };
 
     "org/gnome/desktop/notifications/application/org-gnome-settings" = {
@@ -56,6 +84,10 @@ with lib.hm.gvariant;
       secondary-color = "#000000";
     };
 
+    "org/gnome/evolution-data-server" = {
+      migrated = true;
+    };
+
     "org/gnome/mutter" = {
       experimental-features = [ "scale-monitor-framebuffer" ];
     };
@@ -66,10 +98,24 @@ with lib.hm.gvariant;
 
     "org/gnome/shell" = {
       favorite-apps = [ "firefox.desktop" "org.gnome.Nautilus.desktop" "Alacritty.desktop" "org.gnome.Geary.desktop" "org.gnome.Calendar.desktop" ];
+      welcome-dialog-last-shown-version = "46.4";
     };
 
     "org/gnome/shell/world-clocks" = {
       locations = [];
+    };
+
+    "org/gtk/gtk4/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = false;
+      sidebar-width = 140;
+      sort-column = "name";
+      sort-directories-first = true;
+      sort-order = "ascending";
+      type-format = "category";
+      view-type = "list";
+      window-size = mkTuple [ 859 366 ];
     };
 
     "org/gtk/settings/file-chooser" = {
