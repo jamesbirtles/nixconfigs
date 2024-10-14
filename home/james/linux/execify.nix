0b68f4d424ae09c430851930a77c83b2167cb5ed
James Birtles <james@birtles.dev>
2024-10-14 09:37:44 +0100

    more editor stuff

--- home/james/linux/execify.nix
+++ home/james/linux/execify.nix
@@ -5,6 +5,7 @@
     notion-app-enhanced
     _1password-gui
     zoom-us
+    zed-editor
   ];
   programs.vscode = {
     enable = true;
@@ -13,12 +14,14 @@
     extensions = with vscode-extensions; [
       ms-vsliveshare.vsliveshare
       asvetliakov.vscode-neovim
+      svelte.svelte-vscode
     ];
     userSettings = {
       "window.titleBarStyle" = "custom";
       "extensions.experimental.affinity" = {
         "asvetliakov.vscode-neovim" = 1;
       };
+      "svelte.enable-ts-plugin" = true;
     };
   };
 }
