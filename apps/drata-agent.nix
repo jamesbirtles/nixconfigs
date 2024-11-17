106aa40231cbd4f4e6a14fdb9a98a5e5524e6157
James Birtles <james@birtles.dev>
2024-11-17 15:10:39 +0000

    fully nix drata agent

--- apps/drata-agent.nix
+++ apps/drata-agent.nix
@@ -1,28 +1,27 @@
-{ pkgs, lib, stdenv, ... }:
+{ pkgs, ... }:
 let
-  drata-agent = pkgs.stdenv.mkDerivation rec {
-    name = "drata-agent-desktop-item";
-    dontBuild = true;
-    unpackPhase = "true";
 
-    desktopItem = pkgs.makeDesktopItem {
-      name = "drata-agent";
-      exec = "${pkgs.steam-run}/bin/steam-run \"/home/james/apps/drata-agent/opt/Drata Agent/drata-agent\" %U";
-      # genericName = "Web Browser";
-      # categories = "Application;Network;WebBrowser;";
-      startupWMClass = "Drata Agent";
-      desktopName = "Drata Agent";
-      mimeTypes = [
-        "x-scheme-handler/auth-drata-agent"
-      ];
-    };
+  pname = "drata-agent";
+  version = "3.6.1";
+  name = "${pname}-${version}";
 
-    installPhase = ''
-      mkdir -p $out/share
-      cp -r ${desktopItem}/share/applications $out/share
+  src = pkgs.fetchurl {
+    url = "https://github.com/drata/agent-releases/releases/download/v${version}/Drata-Agent-linux.AppImage";
+    hash = "sha256-ZHwQdw1XXOeiYCrRHffQXCD3uS9FnYBZcjUQah3sLzY=";
+  };
+
+  appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
+
+  drata-agent = pkgs.appimageTools.wrapType2 rec {
+    inherit name pname version src;
+
+    extraInstallCommands = ''
+      install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
+      sed -i 's/AppRun --no-sandbox/drata-agent/g' $out/share/applications/${pname}.desktop
+      cp -r ${appimageContents}/usr/share/icons $out/share
     '';
   };
 in
 {
-  environment.systemPackages = [ pkgs.steam-run drata-agent ];
+  environment.systemPackages = [ drata-agent ];
 }
