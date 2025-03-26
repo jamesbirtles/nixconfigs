5b3bbbb6f3c0813dbe0d1ec2897c45965a25ff49
James Birtles <james@birtles.dev>
2025-03-26 12:43:00 +0000

    upgrade kernel to 6.14

--- shared-linux.nix
+++ shared-linux.nix
@@ -7,7 +7,7 @@
     options = "--delete-older-than 10d";
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_6_11;
+  boot.kernelPackages = pkgs.linuxPackages_6_14;
   boot.loader.systemd-boot.configurationLimit = 10;
 
   networking.networkmanager.enable = true;
