2bb889a49d0a083f434a9ead08f6328ac9edc866
James Birtles <james@birtles.dev>
2025-12-28 16:00:56 +0000

    fix server features

--- modules/profiles/server.nix
+++ modules/profiles/server.nix
@@ -6,7 +6,9 @@
 }:
 {
   features = {
-    shell.zsh.enable = lib.mkDefault true;
-    apps.terminal.enable = lib.mkDefault true;
+    terminal.zsh.enable = lib.mkDefault true;
+    terminal.tools.enable = lib.mkDefault true;
+    development.dev-tools.enable = lib.mkDefault true;
+    media.processing.enable = lib.mkDefault true;
   };
 }
