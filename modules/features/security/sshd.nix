f1c5e27a084090212ec8f717c18918960c753fee
James Birtles <james@birtles.dev>
2025-12-28 17:01:37 +0000

    use defaults for ssh

--- modules/features/security/sshd.nix
+++ modules/features/security/sshd.nix
@@ -31,30 +31,6 @@ in
 
         # Disable X11 forwarding for security
         X11Forwarding = false;
-
-        # Use more secure key exchange algorithms
-        KexAlgorithms = [
-          "curve25519-sha256"
-          "curve25519-sha256@libssh.org"
-          "diffie-hellman-group-exchange-sha256"
-        ];
-
-        # Use more secure ciphers
-        Ciphers = [
-          "chacha20-poly1305@openssh.com"
-          "aes256-gcm@openssh.com"
-          "aes128-gcm@openssh.com"
-          "aes256-ctr"
-          "aes192-ctr"
-          "aes128-ctr"
-        ];
-
-        # Use more secure MACs
-        Macs = [
-          "hmac-sha2-512-etm@openssh.com"
-          "hmac-sha2-256-etm@openssh.com"
-          "umac-128-etm@openssh.com"
-        ];
       };
     };
 
