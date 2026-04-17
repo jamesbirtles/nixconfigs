a9ecb9967a020689b042132c2810b8b2f244a681
James Birtles <james@birtles.dev>
2026-04-17 14:26:31 +0100

    expose ports

--- hosts/jamesb-darwin/default.nix
+++ hosts/jamesb-darwin/default.nix
@@ -11,6 +11,8 @@
 
   networking.hostName = "jamesb-darwin";
 
+  networking.firewall.allowedTCPPorts = [ 4220 4500 ];
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
