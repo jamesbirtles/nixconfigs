6e59a32997e5f6d1e607dcd1004f460806d9e422
James Birtles <james@birtles.dev>
2026-04-27 10:42:42 +0100

    Revert "migrate for firefox change"
    This reverts commit 65dd278fa81c3234e087319a55c37439cda0440b.

--- modules/features/productivity/browsers.nix
+++ modules/features/productivity/browsers.nix
@@ -24,37 +24,34 @@ in
     ];
 
     # Home-manager configuration for user james
-    home-manager.users.james =
-      { config, ... }:
-      {
-        xdg.configFile."mozilla/firefox/default/chrome/firefox-gnome-theme".source =
-          if firefox-gnome-theme != null then
-            firefox-gnome-theme
-          else
-            throw "firefox-gnome-theme input is required when browsers feature is enabled";
+    home-manager.users.james = {
+      home.file.".mozilla/firefox/default/chrome/firefox-gnome-theme".source =
+        if firefox-gnome-theme != null then
+          firefox-gnome-theme
+        else
+          throw "firefox-gnome-theme input is required when browsers feature is enabled";
 
-        programs.firefox.enable = true;
-        programs.firefox.configPath = "${config.xdg.configHome}/mozilla/firefox";
-        programs.firefox.profiles.default = {
-          id = 0;
-          name = "default";
-          isDefault = true;
-          userChrome = ''
-            @import "firefox-gnome-theme/userChrome.css";
-          '';
-          userContent = ''
-            @import "firefox-gnome-theme/userContent.css";
-          '';
-          settings = {
-            # Firefox gnome theme
-            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-            "browser.uidensity" = 0;
-            "svg.context-properties.content.enabled" = true;
-            "browser.theme.dark-private-windows" = false;
-            "widget.gtk.rounded-bottom-corners.enabled" = true;
-            "gnomeTheme.hideSingleTab" = true;
-          };
+      programs.firefox.enable = true;
+      programs.firefox.profiles.default = {
+        id = 0;
+        name = "default";
+        isDefault = true;
+        userChrome = ''
+          @import "firefox-gnome-theme/userChrome.css";
+        '';
+        userContent = ''
+          @import "firefox-gnome-theme/userContent.css";
+        '';
+        settings = {
+          # Firefox gnome theme
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "browser.uidensity" = 0;
+          "svg.context-properties.content.enabled" = true;
+          "browser.theme.dark-private-windows" = false;
+          "widget.gtk.rounded-bottom-corners.enabled" = true;
+          "gnomeTheme.hideSingleTab" = true;
         };
       };
+    };
   };
 }
