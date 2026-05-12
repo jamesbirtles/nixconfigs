c7cf2af86a4cd24f3941c064ceef315ff3f284b2
James Birtles <james@birtles.dev>
2026-05-12 11:16:29 +0100

    add missing port

--- hosts/jamesb-darwin/default.nix
+++ hosts/jamesb-darwin/default.nix
@@ -11,7 +11,7 @@
 
   networking.hostName = "jamesb-darwin";
 
-  networking.firewall.allowedTCPPorts = [ 4221 4500 5349 ];
+  networking.firewall.allowedTCPPorts = [ 4220 4221 4500 5349 ];
   networking.firewall.allowedUDPPorts = [ 3478 5349 ];
 
   boot.loader.systemd-boot.enable = true;
